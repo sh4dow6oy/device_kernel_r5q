466491a14d1b48467984bf67457cdfdd6ff342ea
Steven Rostedt (VMware) <rostedt@goodmis.org>
2018-03-23 10:18:03 -0400

    UPSTREAM: init, tracing: Add initcall trace events
    Being able to trace the start and stop of initcalls is useful to see where
    the timings are an issue. There is already an "initcall_debug" parameter,
    but that can cause a large overhead itself, as the printing of the
    information may take longer than the initcall functions.
    
    Adding in a start and finish trace event around the initcall functions, as
    well as a trace event that records the level of the initcalls, one can get a
    much finer measurement of the times and interactions of the initcalls
    themselves, as trace events are much lighter than printk()s.
    
    Suggested-by: Abderrahmane Benbachir <abderrahmane.benbachir@polymtl.ca>
    Change-Id: I18524f8aa48a9361ddf780854db20e4b933bbd29
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- init/main.c
+++ init/main.c
@@ -115,6 +115,9 @@
 void __init __weak defex_load_rules(void) { }
 #endif
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/initcall.h>
+
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -1127,6 +1130,7 @@ int __init_or_module do_one_initcall(initcall_t fn)
 	if (initcall_blacklisted(fn))
 		return -EPERM;
 
+	trace_initcall_start(fn);
 	if (initcall_debug)
 		ret = do_one_initcall_debug(fn);
 #ifdef CONFIG_SEC_BSP
@@ -1137,6 +1141,7 @@ int __init_or_module do_one_initcall(initcall_t fn)
 #endif
 	else
 		ret = fn();
+	trace_initcall_finish(fn, ret);
 
 	msgbuf[0] = 0;
 
@@ -1201,6 +1206,7 @@ static void __init do_initcall_level(int level)
 		   level, level,
 		   NULL, &repair_env_string);
 
+	trace_initcall_level(initcall_level_names[level]);
 	for (fn = initcall_levels[level]; fn < initcall_levels[level+1]; fn++)
 		do_one_initcall(*fn);
 
@@ -1239,6 +1245,7 @@ static void __init do_pre_smp_initcalls(void)
 {
 	initcall_t *fn;
 
+	trace_initcall_level("early");
 	for (fn = __initcall_start; fn < __initcall0_start; fn++)
 		do_one_initcall(*fn);
 }
