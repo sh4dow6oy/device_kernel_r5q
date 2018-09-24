3e3b0d30de16b71f4c79d54b6fba107eac009fb7
Vlad Buslov <vladbu@mellanox.com>
2018-09-24 19:22:49 +0300

    UPSTREAM: net: core: netlink: add helper refcount dec and lock function
    Rtnl lock is encapsulated in netlink and cannot be accessed by other
    modules directly. This means that reference counted objects that rely on
    rtnl lock cannot use it with refcounter helper function that atomically
    releases decrements reference and obtains mutex.
    
    This patch implements simple wrapper function around refcount_dec_and_lock
    that obtains rtnl lock if reference counter value reached 0.
    
    Change-Id: I526173ad18fe45c2aaa78049f8afedaaa3dad811
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/rtnetlink.h
+++ include/linux/rtnetlink.h
@@ -32,6 +32,7 @@ extern void rtnl_unlock(void);
 extern int rtnl_trylock(void);
 extern int rtnl_is_locked(void);
 extern int rtnl_lock_killable(void);
+extern bool refcount_dec_and_rtnl_lock(refcount_t *r);
 
 extern wait_queue_head_t netdev_unregistering_wq;
 extern struct mutex net_mutex;
