2ab9d9df408295814f37ba41e702c75856d28f30
Rick Yiu <rickyiu@google.com>
2019-03-13 15:48:03 +0800

    BACKPORT: sched/fair: if sync flag ignored, try to place in mid cluster
    If the sync flag is ignored because the current cpu is not in
    the affinity mask for the target of a sync wakeup (usually
    binder call), prefer to place in the mid cluster if
    possible. The main case is a "top-app" task waking a
    "foreground" task when the top-app task is running on
    a CPU that is not in the foreground cpuset. This patch
    causes the search order from mid capacity cpu be used
    when the sync flag failed.
    
    backport from commit 98ae57d9eaf7
    ("ANDROID: sched/fair: if sync flag ignored, try to place in same cluster")
    
    Bug: 117438867
    Test: boot to home, operation normal
    Change-Id: I68d0cc05db1bc2cb02d4445c71b02215209e8c04
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7662,8 +7662,7 @@ enum fastpaths {
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool boosted, bool sync_boost,
 				   bool prefer_idle,
-				   struct find_best_target_env *fbt_env,
-				   bool prefer_high_cap)
+				   struct find_best_target_env *fbt_env)
 {
 	unsigned long min_util = boosted_task_util(p);
 	unsigned long target_capacity = ULONG_MAX;
@@ -7705,7 +7704,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		target_capacity = 0;
 
 	/* Find start CPU based on boost value */
-	cpu = start_cpu(p, prefer_high_cap, sync_boost, fbt_env->rtg_target);
+	cpu = start_cpu(p, boosted, sync_boost, fbt_env->rtg_target);
 	if (cpu < 0)
 		return -1;
 
@@ -8379,7 +8378,8 @@ static inline struct cpumask *find_rtg_target(struct task_struct *p)
 static int find_energy_efficient_cpu(struct sched_domain *sd,
 				     struct task_struct *p,
 				     int cpu, int prev_cpu,
-				     int sync, bool sync_boost)
+				     int sync, int sibling_count_hint,
+				     bool sync_boost)
 {
 	int use_fbt = sched_feat(FIND_BEST_TARGET);
 	int cpu_iter, eas_cpu_idx = EAS_CPU_NXT;
@@ -8474,7 +8474,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 		/* Find a cpu with sufficient capacity */
 		target_cpu = find_best_target(p, &eenv->cpu[EAS_CPU_BKP].cpu_id,
 					      boosted, sync_boost, prefer_idle,
-					      &fbt_env, prefer_high_cap);
+					      &fbt_env);
 		if (target_cpu < 0)
 			goto out;
 
@@ -8702,14 +8702,12 @@ pick_cpu:
 			 * indicate that the selection algorithm from mid
 			 * capacity cpu should be used.
 			*/
-			int high_cap_cpu =
-			    cpu_rq(cpu)->rd->mid_cap_orig_cpu != -1 ?
-			     cpu_rq(cpu)->rd->mid_cap_orig_cpu :
-			     cpu_rq(cpu)->rd->max_cap_orig_cpu;
-			bool sync_boost = sync && cpu >= high_cap_cpu;
+			bool sync_boost = sync &&
+				      cpu >= cpu_rq(cpu)->rd->mid_cap_orig_cpu;
 
 			new_cpu = find_energy_efficient_cpu(energy_sd, p, cpu,
-						    prev_cpu, sync, sync_boost);
+					prev_cpu, sync, sibling_count_hint,
+					sync_boost);
 		}
 
 		/* if we did an energy-aware placement and had no choices available
@@ -13374,7 +13372,7 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 		raw_spin_lock(&migration_lock);
 		rcu_read_lock();
 		new_cpu = find_energy_efficient_cpu(sd, p, cpu, prev_cpu,
-						    0, false);
+				0, 1, false);
 		rcu_read_unlock();
 		if ((new_cpu != -1) &&
 			(capacity_orig_of(new_cpu) > capacity_orig_of(cpu))) {
