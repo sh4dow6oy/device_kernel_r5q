b1b23c04cc262ca318a04c4546fff231df2677d3
Rick Yiu <rickyiu@google.com>
2020-03-25 21:41:10 +0800

    sched: separate capacity margin for boosted tasks
    With the introduction of placement hint patch, boosted tasks will not
    scheduled from big cores. We tune capacity margin to let important
    boosted tasks get scheduled on big cores. However, the capacity margin
    affects all group of tasks, so that non-boosted tasks get more chances
    to be scheduled on big cores, too. This could be solved by separating
    capacity margin for boosted tasks.
    
    Bug: 147785606
    Test: margin set correctly
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I2b02e138e36a6844afbc1ade60fe86a001814b30

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -72,6 +72,10 @@ enum sched_tunable_scaling {
 };
 extern enum sched_tunable_scaling sysctl_sched_tunable_scaling;
 
+extern int sched_updown_migrate_handler_boosted(struct ctl_table *table,
+					int write, void __user *buffer,
+					size_t *lenp, loff_t *ppos);
+
 extern unsigned int sysctl_numa_balancing_scan_delay;
 extern unsigned int sysctl_numa_balancing_scan_period_min;
 extern unsigned int sysctl_numa_balancing_scan_period_max;
