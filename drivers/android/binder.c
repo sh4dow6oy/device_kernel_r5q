4e8524fa6f3308d5d0b8dc55820862cb42b7ca57
Todd Kjos <tkjos@google.com>
2021-10-12 09:56:14 -0700

    UPSTREAM: binder: use cred instead of task for getsecid
    commit 4d5b5539742d2554591751b4248b0204d20dcc9d upstream.
    
    Use the 'struct cred' saved at binder_open() to lookup
    the security ID via security_cred_getsecid(). This
    ensures that the security context that opened binder
    is the one used to generate the secctx.
    
    Cc: stable@vger.kernel.org # 5.4+
    Fixes: ec74136ded79 ("binder: create node flag to request sender's security context")
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Suggested-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Change-Id: Ia7b59804a0bdbd51191bbcca556414840307c623
    Bug: 200688826
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3331,7 +3331,10 @@ static void binder_transaction(struct binder_proc *proc,
 		int retries = 0;
 		int max_retries = 100;
 
+<<<<<<< HEAD
 retry_lowmem:
+=======
+>>>>>>> 06906ab78832 (UPSTREAM: binder: use cred instead of task for getsecid)
 		security_cred_getsecid(proc->cred, &secid);
 		ret = security_secid_to_secctx(secid, &secctx, &secctx_sz);
 		if (ret == -ENOMEM && retries++ < max_retries) {
