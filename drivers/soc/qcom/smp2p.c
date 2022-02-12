d1c6b7241e08590c3d167f3da949062a8c0eefad
Sultan Alsawaf <sultan@kerneltoast.com>
2022-02-12 14:58:18 -0800

    Revert "soc: qcom: smp2p: Prevent suspend for threaded irq"
    This reverts commit 9dcdb6f5ee0ea133f2e0d669743fcb48362ee4c5.
    
    The IRQ subsystem already blocks suspend on waiting for IRQ threads to
    finish running (in dpm_noirq_begin()). This PM wakeup does nothing but add
    latency to the IRQ handler for non-RT kernels, and it isn't RT-friendly
    either:
    [   42.466403] BUG: sleeping function called from invalid context at kernel/locking/rtmutex.c:974
    [   42.466407] in_atomic(): 1, irqs_disabled(): 128, pid: 0, name: swapper/3
    [   42.466408] Preemption disabled at:
    [   42.466421] [<00000000100c9f7d>] secondary_start_kernel+0xa8/0x130
    [   42.466427] CPU: 3 PID: 0 Comm: swapper/3 Tainted: G S      W       4.14.212-rt102-Sultan #1
    [   42.466429] Hardware name: Qualcomm Technologies, Inc. SM8150 V2 PM8150 Google Inc. MSM sm8150 Coral (DT)
    [   42.466432] Call trace:
    [   42.466436]  dump_backtrace+0x0/0x1ac
    [   42.466439]  show_stack+0x14/0x1c
    [   42.466444]  dump_stack+0x84/0xac
    [   42.466448]  ___might_sleep+0x140/0x150
    [   42.466452]  rt_spin_lock+0x3c/0x50
    [   42.466458]  __pm_stay_awake+0x20/0x50
    [   42.466462]  qcom_smp2p_isr+0x10/0x1c
    [   42.466467]  __handle_irq_event_percpu+0x60/0xd4
    [   42.466469]  handle_irq_event_percpu+0x58/0xb0
    [   42.466471]  handle_irq_event+0x68/0xe0
    [   42.466474]  handle_fasteoi_irq+0x140/0x1fc
    [   42.466476]  generic_handle_irq+0x18/0x2c
    [   42.466478]  __handle_domain_irq+0xf8/0xfc
    [   42.466481]  gic_handle_irq+0xc8/0x164
    [   42.466483]  el1_irq+0xb0/0x130
    [   42.466487]  finish_task_switch+0xcc/0x1e4
    [   42.466491]  __schedule+0x3f0/0x4e0
    [   42.466493]  schedule_idle+0x28/0x44
    [   42.466497]  do_idle+0x78/0x230
    [   42.466500]  cpu_startup_entry+0x20/0x28
    [   42.466502]  secondary_start_kernel+0x124/0x130
    
    Remove it since it's useless.
    
    Change-Id: Ib4a03d89bbaaf114980ee93105ce3d5b0b5127eb
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/soc/qcom/smp2p.c
+++ drivers/soc/qcom/smp2p.c
@@ -26,7 +26,6 @@
 #include <linux/soc/qcom/smem.h>
 #include <linux/soc/qcom/smem_state.h>
 #include <linux/spinlock.h>
-#include <linux/pm_wakeup.h>
 
 #include <linux/ipc_logging.h>
 
@@ -165,7 +164,6 @@ struct qcom_smp2p {
 	struct regmap *ipc_regmap;
 	int ipc_offset;
 	int ipc_bit;
-	struct wakeup_source ws;
 
 	struct mbox_client mbox_client;
 	struct mbox_chan *mbox_chan;
@@ -302,14 +300,6 @@ static void qcom_smp2p_notify_in(struct qcom_smp2p *smp2p)
 	}
 }
 
-static irqreturn_t qcom_smp2p_isr(int irq, void *data)
-{
-	struct qcom_smp2p *smp2p = data;
-
-	__pm_stay_awake(&smp2p->ws);
-	return IRQ_WAKE_THREAD;
-}
-
 /**
  * qcom_smp2p_intr() - interrupt handler for incoming notifications
  * @irq:	unused
@@ -334,7 +324,7 @@ static irqreturn_t qcom_smp2p_intr(int irq, void *data)
 		if (IS_ERR(in)) {
 			dev_err(smp2p->dev,
 				"Unable to acquire remote smp2p item\n");
-			goto out;
+			return IRQ_HANDLED;
 		}
 
 		smp2p->in = in;
@@ -353,8 +343,6 @@ static irqreturn_t qcom_smp2p_intr(int irq, void *data)
 			qcom_smp2p_do_ssr_ack(smp2p);
 	}
 
-out:
-	__pm_relax(&smp2p->ws);
 	return IRQ_HANDLED;
 }
 
@@ -605,11 +593,6 @@ static int qcom_smp2p_alloc_item(struct platform_device *pdev,
 			list_add(&entry->node, &smp2p->outbound);
 		}
 	}
-#if IS_ENABLED(CONFIG_SEC_PM)
-	wakeup_source_init(&smp2p->ws, smp2p->name);
-#else
-	wakeup_source_init(&smp2p->ws, "smp2p");
-#endif
 
 	/* Kick the outgoing edge after allocating entries */
 	qcom_smp2p_kick(smp2p);
@@ -703,7 +686,7 @@ static int qcom_smp2p_probe(struct platform_device *pdev)
 					smp2p->name, (void *)smp2p);
 #else
 	ret = devm_request_threaded_irq(&pdev->dev, smp2p->irq,
-					qcom_smp2p_isr, qcom_smp2p_intr,
+					NULL, qcom_smp2p_intr,
 					IRQF_ONESHOT,
 					"smp2p", (void *)smp2p);
 #endif
@@ -783,7 +766,6 @@ static int qcom_smp2p_restore(struct device *dev)
 			list_add(&entry->node, &smp2p->outbound);
 		}
 	}
-	wakeup_source_init(&smp2p->ws, "smp2p");
 
 	enable_irq_wake(smp2p->irq);
 	/* Kick the outgoing edge after allocating entries */
@@ -818,8 +800,6 @@ static int qcom_smp2p_freeze(struct device *dev)
 	/* make null to point it to valid smem item during first interrupt */
 	smp2p->in = NULL;
 	smp2p->valid_entries = 0;
-	/* remove wakeup source */
-	wakeup_source_trash(&smp2p->ws);
 	return 0;
 }
 
