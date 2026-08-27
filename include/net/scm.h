eb281a1ae5acae988581096cc0104eb221b076a1
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-08-27 14:48:04 +0300

    Remove unused include for linux/sched/signal.h

--- include/net/scm.h
+++ include/net/scm.h
@@ -8,7 +8,6 @@
 #include <linux/security.h>
 #include <linux/pid.h>
 #include <linux/nsproxy.h>
-#include <linux/sched/signal.h>
 
 /* Well, we should have at least one descriptor open
  * to accept passed FDs 8)
@@ -153,4 +152,3 @@ static __inline__ void scm_recv(struct socket *sock, struct msghdr *msg,
 
 
 #endif /* __LINUX_NET_SCM_H */
-
