3d01a77812cf49d38beb42cf70e5b5c115ea2a2c
AlfCraft07 <132297876+AlfCraft07@users.noreply.github.com>
2026-09-07 17:17:44 +0200

    include/linux: add compatibility stub for DMA_MAP_OPS

---
+++ include/linux/dma-map-ops.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_DMA_MAP_OPS_H
+#define _LINUX_DMA_MAP_OPS_H
+
+#include <linux/dma-mapping.h>
+#include <linux/dma-contiguous.h>
+
+#endif /* _LINUX_DMA_MAP_OPS_H */
