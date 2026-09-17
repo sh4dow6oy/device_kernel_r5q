9c212642920ac117767960d6821974ad3549de3a
AlfCraft07 <132297876+AlfCraft07@users.noreply.github.com>
2026-09-17 18:34:29 +0200

    BACKPORT: bpf: fixes after cherrypicking

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -1127,6 +1127,7 @@ int __cgroup_bpf_run_filter_sock_addr(struct sock *sk,
 		.t_ctx = t_ctx,
 	};
 	struct sockaddr_storage unspec;
+	struct bpf_prog_array *prog_array;
 	struct cgroup *cgrp;
 	int ret;
 
@@ -1142,7 +1143,6 @@ int __cgroup_bpf_run_filter_sock_addr(struct sock *sk,
 	}
 
 	cgrp = sock_cgroup_ptr(&sk->sk_cgrp_data);
-	ret = BPF_PROG_RUN_ARRAY(cgrp->bpf.effective[type], &ctx, BPF_PROG_RUN);
 
 	if (unlikely(!cgrp))
 		return 0;
