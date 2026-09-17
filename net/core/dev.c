8076a0be10ec41010b80a1dd51413bdb11e0b7c6
AlfCraft07 <132297876+AlfCraft07@users.noreply.github.com>
2026-09-17 19:56:09 +0200

    net: dev.c: remove implicit declaration

--- net/core/dev.c
+++ net/core/dev.c
@@ -4213,7 +4213,7 @@ static u32 netif_receive_generic_xdp(struct sk_buff *skb,
 	/* Reinjected packets coming from act_mirred or cpumap should
 	 * not get XDP generic processing a second time.
 	 */
-	if (skb_is_tc_redirected(skb) || skb_is_redirected(skb))
+	if (skb_is_tc_redirected(skb))
 		return XDP_PASS;
 
 	/* XDP packets must be linear and must have sufficient headroom
