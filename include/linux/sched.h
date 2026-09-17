282f6ff951508807aaa2a68f738b1b2521a00717
AlfCraft07 <132297876+AlfCraft07@users.noreply.github.com>
2026-09-17 18:07:36 +0200

    include: linux: add android_kabi.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -27,6 +27,7 @@
 #include <linux/signal_types.h>
 #include <linux/mm_types_task.h>
 #include <linux/task_io_accounting.h>
+#include <linux/android_kabi.h
 
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
