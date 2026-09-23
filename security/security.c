0b80a8d0cc895d445f205a3f8a40ae327e7488b8
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-09-23 08:10:06 +0300

    Remove redundant security checks from security.c
    Removed calls to five_bprm_check, five_inode_setxattr, five_inode_removexattr, and five_file_open functions.

--- security/security.c
+++ security/security.c
@@ -777,9 +777,6 @@ int security_bprm_check(struct linux_binprm *bprm)
 	ret = call_int_hook(bprm_check_security, 0, bprm);
 	if (ret)
 		return ret;
-	ret = five_bprm_check(bprm);
-	if (ret)
-		return ret;
 	return ima_bprm_check(bprm);
 }
 
@@ -1211,9 +1208,6 @@ int security_inode_setxattr(struct dentry *dentry, const char *name,
 		ret = cap_inode_setxattr(dentry, name, value, size, flags);
 	if (ret)
 		return ret;
-	ret = five_inode_setxattr(dentry, name, value, size);
-	if (ret)
-		return ret;
 	ret = ima_inode_setxattr(dentry, name, value, size);
 	if (ret)
 		return ret;
@@ -1256,9 +1250,6 @@ int security_inode_removexattr(struct dentry *dentry, const char *name)
 	ret = call_int_hook(inode_removexattr, 1, dentry, name);
 	if (ret == 1)
 		ret = cap_inode_removexattr(dentry, name);
-	if (ret)
-		return ret;
-	ret = five_inode_removexattr(dentry, name);
 	if (ret)
 		return ret;
 	ret = ima_inode_removexattr(dentry, name);
@@ -1493,11 +1484,7 @@ int security_file_open(struct file *file)
 	if (ret)
 		return ret;
 
-	ret = fsnotify_perm(file, MAY_OPEN);
-	if (ret)
-		return ret;
-
-	return five_file_open(file);
+	return fsnotify_perm(file, MAY_OPEN);
 }
 
 int security_task_alloc(struct task_struct *task, unsigned long clone_flags)
